afb04fe2bedbf1bcf576906b19e208d8e1bd9dd1
git-arbitrarysystems <^_^@arbitrarysystems.com>
2020-11-05 14:12:54 +0100

    working on purging

--- nginx/remove-cache-by-key.sh
+++ nginx/remove-cache-by-key.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo $("rm -rf /opt/nginx-fcgi-cache/$1")
+# echo "yo" >&2
+# ls /opt/nginx-fcgi-cache/ >&2
+# echo $("rm -rf /opt/nginx-fcgi-cache/$1") >&2
+
+
+echo ">>>>>>>>>>>>>>>>>> Purge request: path:$1 args:$2" >&2
+
+# rm -rf $(grep -lr "$1" /opt/nginx-fcgi-cache/) >&2
