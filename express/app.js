779a4d863a8403870509157619bcf72e9880f77d
jasper.funk-smit <jasper.funk-smit@mfn-berlin.de>
2021-06-11 14:33:46 +0200

    added error handling for unavailable images

--- express/app.js
+++ express/app.js
@@ -1,10 +1,14 @@
 const express = require("express"),
   stream = require("stream"),
   http = require("http"),
+  https = require("https"),
   fs = require("fs"),
-  cors = require("cors");
+  cors = require("cors"),
+  isReachable = require("is-reachable");
 
-const THUMBNAIL_WIDTH = process.env.THUMBNAIL_WIDTH || "400";
+const THUMBNAIL_WIDTH = process.env.THUMBNAIL_WIDTH || "400",
+  REACHABLE_TIMEOUT = Number(process.env.REACHABLE_TIMEOUT || "5000"),
+  RENDER_TIMEOUT = Number(process.env.RENDER_TIMEOUT || "10000");
 
 /**
  * Capture close
@@ -34,49 +38,85 @@ if (!fs.existsSync(THUMBS)) {
  *
  *
  */
-const renderAndGetImage = (url, filename, res, next) => {
+const renderAndGetImage = async (url, filename, res, next) => {
+  const failed = (o) => {
+    console.log("Failed.", o);
+    next(o);
+  };
+
   /**
-   * Query the h2non/imaginary server
-   * */
-  http
-    .request(url, function (http_res) {
-      const { statusCode } = http_res,
-        contentType = http_res.headers["content-type"];
+   * Check if the url is reachable
+   */
+  const reachable = await isReachable(url, { timeout: REACHABLE_TIMEOUT });
+  if (reachable) {
+    /**
+     * Query the imaginary server
+     */
+    const options = {
+      protocol: "http:",
+      host: "imaginary",
+      port: 9000,
+      path: `/resize?width=${THUMBNAIL_WIDTH}&url=${encodeURIComponent(url)}`,
+      timeout: RENDER_TIMEOUT,
+    };
+
+    const request = http.request(options, function (response) {
+      const { statusCode } = response,
+        contentType = response.headers["content-type"];
 
       if (statusCode !== 200) {
         /** Image probably doesn't exist */
-        next(new Error(`Request ${url} Failed.\nStatus Code: ${statusCode}`));
+        failed(new Error(`Invalid Status Code: ${statusCode}`));
       } else if (!/^image\/.*/.test(contentType)) {
         /** Probably a json was returned mentioning some missing param */
-        next(
-          new Error(
-            `Invalid content-type returned from url:${url}.\nExpected image/* but received ${contentType}.`
-          )
-        );
+        failed(new Error(`Invalid content-type: ${contentType}.`));
       } else {
         /** Start a stream */
         const data = new stream.Transform();
 
         /** Collect chunks */
-        http_res.on("data", function (chunk) {
+        response.on("data", function (chunk) {
           data.push(chunk);
         });
 
         /** Wait for data to complete */
-        http_res.on("end", function () {
+        response.on("end", function () {
+          console.log("Render complete. Writing image");
           fs.writeFile(filename, data.read(), (err) => {
             if (err) {
               /** File writing error */
-              next(err);
+              failed(err);
             } else {
               /** All is well, return the image */
+              console.log("Success!");
               res.sendFile(filename);
             }
           });
         });
       }
-    })
-    .end();
+    });
+
+    request.on("timeout", function () {
+      request.destroy();
+      failed(
+        new Error(
+          `Request to imaginary:9000 timed out. RENDER_TIMEOUT:${RENDER_TIMEOUT}`
+        )
+      );
+    });
+
+    request.on("error", (error) => {
+      failed(new Error("Unhandled error"));
+    });
+
+    request.end();
+  } else {
+    failed(
+      new Error(
+        `${url} is not reachable. REACHABLE_TIMEOUT:${REACHABLE_TIMEOUT}`
+      )
+    );
+  }
 };
 
 /**
@@ -120,8 +160,8 @@ app.get("/thumbnails", async (req, res, next) => {
 app.get("/thumbnail", async (req, res, next) => {
   const { params, query } = req;
 
-  const encoded = encodeURIComponent(decodeURIComponent(query.url)),
-    url = `http://imaginary:9000/resize?width=${THUMBNAIL_WIDTH}&url=${encoded}`,
+  const url = decodeURIComponent(query.url),
+    encoded = encodeURIComponent(url),
     filename = `${THUMBS}/${encoded}`;
 
   if (!query.hasOwnProperty("force") && fs.existsSync(filename)) {
@@ -129,7 +169,7 @@ app.get("/thumbnail", async (req, res, next) => {
     res.sendFile(filename);
   } else {
     console.log(
-      "Rendering image",
+      "Requesting to render image",
       query.hasOwnProperty("force") ? "(force)" : "",
       filename
     );
